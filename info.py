773ea551f1a099e6a0846770c03295c141b0baa8
kwicbot78 <115940699+kwicbot78@users.noreply.github.com>
2022-11-13 14:46:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '5474689804:AAFQv7-eR9mfNtsmWy_ZOpUTLImJfhb
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/e00a46f1ee27288a1810a.jpg')).split()
+PICS = (environ.get('PICS', 'https://te.legra.ph/file/8dfe7256883cbc0190478.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '942099709').split()]
